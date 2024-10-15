d40094240e94555e98073effe4a4b9f75daa4537
jainavani21-cloud <d2022.jai.navani@ves.ac.in>
2024-10-15 22:27:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+      id: 1,
+      name: 'mercedes benz',
+      image: 'https://i.postimg.cc/hP3d4g7b/a-class-limousine-exterior-right-front-three-quarter-7.webp',
+      price: 399.95,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 2,
+      name: 'mercedes SUV',
+      image: 'https://i.postimg.cc/8cZr6gXG/MB.avif',
+      price: 299.98,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 3,
+      name: 'mercedes AMG',
+      image: 'https://i.postimg.cc/gJJp09h8/mercedes-amg-g-63.jpg',
+      price: 1000.99,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 4,
+      name: 'Maybach',
+      image: 'https://i.postimg.cc/ZRMSBfPg/hq720.jpg',
+      price: 999.99,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+  ]
+  const people = [
+    { id: 1, name: 'john' },
+    { id: 2, name: 'peter' },
+    { id: 3, name: 'susan' },
+    { id: 4, name: 'anna' },
+    { id: 5, name: 'emma' },
+  ]
+  module.exports = { products, people }
